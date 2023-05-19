e9cd26f85fba257c9610b779cdaa7aad17e7a564
hossensumon071 <55638643+hossensumon071@users.noreply.github.com>
2023-05-20 02:12:00 +0600

    courses card added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["plus.unsplash.com", "www.devteam.space", "images.unsplash.com", "ahaslides.com"]
+  }
 }
 
 module.exports = nextConfig
--- pages/courses/index.js
+++ pages/courses/index.js
@@ -1,8 +1,40 @@
+import CourseItem from "@/components/CourseItem";
+import SectionHeader from "@/components/SectionHeader";
+import { getAllCourses } from "@/prisma/courses";
 
-const CoursesPage = () => {
+const CoursesPage = ({ courses }) => {
   return (
-    <div>CoursesPage</div>
-  )
-}
+    <div className="wrapper py-10">
+      <SectionHeader
+        span="Courses"
+        h2="Browse all courses"
+        p="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+          minima vero officiis deserunt amet tenetur."
+      />
 
-export default CoursesPage
+      <div className="mt-10 flex flex-wrap gap-10">
+        {courses.map((course) => (
+          <CourseItem key={course.id} course={course} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CoursesPage;
+
+export const getServerSideProps = async () => {
+  const courses = await getAllCourses();
+
+  const updatedCourses = courses.map((course) => ({
+    ...course,
+    updatedAt: course.updatedAt.toString(),
+    createdAt: course.createdAt.toString(),
+  }));
+
+  return {
+    props: {
+      courses: updatedCourses,
+    },
+  };
+};
